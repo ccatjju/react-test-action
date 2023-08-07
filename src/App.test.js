38c42f8d585195b08152b880f1930a49e6e85e80
sulbum yun <sulbumyun@sulbumui-MacBookPro.local>
2023-08-07 09:49:57 +0900

    Is anyone watching?

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,42 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("count start at 0", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const countElement = screen.getByTestId("counter");
+  expect(countElement).toHaveTextContent(0);
+});
+
+test("minus button has correct text", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("minus-button");
+  expect(buttonElement).toHaveTextContent("-");
+});
+
+test("plus button has correct text", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  expect(buttonElement).toHaveTextContent("+");
+});
+
+test("when plus button pressed, the counter change", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  fireEvent.click(buttonElement);
+  const countElement = screen.getByTestId("counter");
+  expect(countElement).toHaveTextContent(1);
+});
+
+test("when on/off button pressed, check the change backgroundclor", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("on/off-button");
+  expect(buttonElement).toHaveStyle({ backgroundColor: "blue" });
+});
+
+test("prevent on/off button, from being pressed when on/off butoon is click", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("on/off-button");
+  fireEvent.click(buttonElement);
+  const plusButtonElement = screen.getByTestId("plus-button");
+  expect(plusButtonElement).toBeDisabled();
 });
